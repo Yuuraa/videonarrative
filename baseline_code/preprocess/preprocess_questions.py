ae5c382951481b633701e5033b657b5bcb7e1eda
yura <yoorachoi8937@gmail.com>
2021-12-15 13:43:58 +0900

    Bert Tokenizer Encoder (sentence tokens to idx)

--- baseline_code/preprocess/preprocess_questions.py
+++ baseline_code/preprocess/preprocess_questions.py
@@ -2,52 +2,37 @@ import argparse
 import numpy as np
 import os
 
-from datautils import video_narr
+from torch._C import Value
+
+# from datautils import video_narr
+from datautils import video_narr_bert
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', default='tgif-qa', choices=['tgif-qa', 'msrvtt-qa', 'msvd-qa','video-narr'], type=str)
+    parser.add_argument('--dataset', default='video-narr-bert', choices=['video-narr', 'video-narr-bert'], type=str)
     parser.add_argument('--answer_top', default=4000, type=int)
     parser.add_argument('--glove_pt',
                         help='glove pickle file, should be a map whose key are words and value are word vectors represented by numpy arrays. Only needed in train mode')
-    parser.add_argument('--output_pt', type=str, default='data/{}/{}_{}_questions.pt')
-    parser.add_argument('--vocab_json', type=str, default='data/{}/{}_vocab.json')
-    parser.add_argument('--mode', choices=['train', 'val', 'test'])
+    parser.add_argument('--output_pt', type=str, default='/mnt/disk1/video_narr/{}/{}_{}_questions.pt')
+    parser.add_argument('--vocab_json', type=str, default='/mnt/disk1/video_narr/{}/{}_vocab.json')
+    parser.add_argument('--mode', default='train', choices=['train', 'val', 'test'])
     parser.add_argument('--question_type', choices=['frameqa', 'action', 'transition', 'count', 'none'], default='none')
     parser.add_argument('--seed', type=int, default=666)
-    parser.add_argument('--video_dir', help='base directory of data' )
+    parser.add_argument('--video_dir', default='/mnt/disk1/video_narr/raw_data', help='base directory of data' )
 
     args = parser.parse_args()
     np.random.seed(args.seed)
 
     # 본 경진대회는 "video-narr" 데이터셋 사용
-    if args.dataset == 'tgif-qa':
-        args.annotation_file = '/home/tgif-qa-master/dataset/{}_{}_question.csv' # args.annotation_file = '/ceph-g/lethao/datasets/tgif-qa/csv/{}_{}_question.csv'
-        args.output_pt = 'data/tgif-qa/{}/tgif-qa_{}_{}_questions.pt'
-        args.vocab_json = 'data/tgif-qa/{}/tgif-qa_{}_vocab.json'
-        # check if data folder exists
-        if not os.path.exists('data/tgif-qa/{}'.format(args.question_type)):
-            os.makedirs('data/tgif-qa/{}'.format(args.question_type))
-
-        if args.question_type in ['frameqa', 'count']:
-            tgif_qa.process_questions_openended(args)
-        else:
-            tgif_qa.process_questions_mulchoices(args)
-    elif args.dataset == 'msrvtt-qa':
-        args.annotation_file = '/ceph-g/lethao/datasets/msrvtt/annotations/{}_qa.json'.format(args.mode)
-        # check if data folder exists
-        if not os.path.exists('data/{}'.format(args.dataset)):
-            os.makedirs('data/{}'.format(args.dataset))
-        msrvtt_qa.process_questions(args)
-    elif args.dataset == 'msvd-qa':
-        args.annotation_file = '/ceph-g/lethao/datasets/msvd/MSVD-QA/{}_qa.json'.format(args.mode)
-        # check if data folder exists
-        if not os.path.exists('data/{}'.format(args.dataset)):
-            os.makedirs('data/{}'.format(args.dataset))
-        msvd_qa.process_questions(args)
-    elif args.dataset == 'video-narr':
-        args.output_pt = 'data/{}/{}_{}_questions.pt'
-        args.vocab_json = 'data/{}/{}_vocab.json'
-        if not os.path.exists('data/{}'.format(args.dataset)):
-            os.makedirs('data/{}'.format(args.dataset))
+    if args.dataset == 'video-narr':
+        if not os.path.exists('/mnt/disk1/video_narr/{}'.format(args.dataset)):
+            os.makedirs('/mnt/disk1/video_narr/{}'.format(args.dataset))
         video_narr.process_questions_mulchoices(args)
+
+    elif args.dataset == 'video-narr-bert':
+        if not os.path.exists('/mnt/disk1/video_narr/{}'.format(args.dataset)):
+            os.makedirs('/mnt/disk1/video_narr/{}'.format(args.dataset))
+        video_narr_bert.process_questions_mulchoices(args)
+    
+    else:
+        raise ValueError(f"No such dataset defined: {args.dataset}")
